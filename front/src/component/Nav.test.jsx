cb80bc362bd35459b0a203cffd61f85d272bc5d6
Peterlits Zo <peterlitszo@gmail.com>
2022-01-27 00:56:32 +0800

    test/fix: Add test for localStorage and make test pass, etc.
    We use `@testing-library/react` to test. Because it have more methods that only
    DOM element.
    
    We also add test for localStorage.
    
    By the way, we fixed the bug -- It cannot show the right mode but just normal
    mode. We will add a test next commit.
    
    We use a long time to fix the bug that the data changed after we test it -- and
    then the test failed. >_< sad. I record it [here][my-answer].
    
    [my-answer]: https://stackoverflow.com/a/70867004/13031497

--- front/src/component/Nav.test.jsx
+++ front/src/component/Nav.test.jsx
@@ -1,62 +1,58 @@
 import React from 'react';
-import { unmountComponentAtNode, render } from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
+import { Provider } from 'react-redux';
+
+import store from '../store/store.js';
 
 import Nav from './Nav.jsx';
 
-let container = null;
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-  localStorage.clear();
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-test('Nav all', () => {
+test('Nav all', async () => {
   // This will only build one child like `<nav>...</nav>`. And is well set not
   // dark as default.
-  expect(localStorage.isDark).toBe(undefined);
-  act(() => {
-    render(<Nav />, container);
-  });
-  expect(container.children.length).toBe(1);
-  expect(localStorage.isDark).toBe('false');
+  expect(localStorage.getItem('isDark')).toBe('false');
+  const { getByText, container } = render(
+    <Provider store={store}>
+      <Nav />
+    </Provider>
+  );
 
-  const content = container.children[0];
-  expect(content.nodeName).toBe('NAV');
-
-  // The nav will have three children: log, darkModeLabel, darkModelToggle.
-  expect(content.children.length).toBe(3);
-
-  const [logo, darkMode, Menu] = content.children;
+  // There will have a logo. And it is the first child of its parent.
+  const logo = getByText('ACM Homepage');
   expect(logo.nodeName).toBe('SPAN');
-  expect(logo.innerHTML).toBe('ACM Homepage');
-  expect(darkMode.nodeName).toBe('DIV');
-  const [darkModeLabel, darkModelToggle] = darkMode.children;
 
-  // `darkModelToggle` can change the root `<html>...</html>` to have or not
+  // Test the nav.
+  const nav = logo.parentNode;
+  expect(nav.nodeName).toBe('NAV');
+  expect(nav.children.length).toBe(3);
+  expect(nav.children[0]).toBe(logo);
+
+  // Split the dark mode.
+  const [_, darkMode, menu] = nav.children;
+  const [darkModelLabel, darkModeToggle] = darkMode.children;
+
+  // `darkModeToggle` can change the root `<html>...</html>` to have or not
   // have class `dark`.
-  expect(darkModelToggle.nodeName).toBe('BUTTON');
+  expect(darkModeToggle.nodeName).toBe('BUTTON');
   expect(document.documentElement.classList.contains('dark')).toBe(false);
-  expect(localStorage.isDark).toBe('false');
-  act(() => darkModelToggle.click());
-  expect(document.documentElement.classList.contains('dark')).toBe(true);
-  expect(localStorage.isDark).toBe('true');
-  act(() => darkModelToggle.click());
-  expect(document.documentElement.classList.contains('dark')).toBe(false);
-  expect(localStorage.isDark).toBe('false');
-  act(() => darkModelToggle.click());
-  expect(document.documentElement.classList.contains('dark')).toBe(true);
-  expect(localStorage.isDark).toBe('true');
-  act(() => darkModelToggle.click());
-  expect(document.documentElement.classList.contains('dark')).toBe(false);
-  expect(localStorage.isDark).toBe('false');
 
-  // And There have a button `Menu`
-  expect(Menu.nodeName).toBe('DIV');
+  fireEvent.click(darkModeToggle);
+  await new Promise(process.nextTick);
+  expect(document.documentElement.classList.contains('dark')).toBe(true);
+  expect(localStorage.getItem('isDark')).toBe('true');
+
+  fireEvent.click(darkModeToggle);
+  await new Promise(process.nextTick);
+  expect(document.documentElement.classList.contains('dark')).toBe(false);
+  expect(localStorage.getItem('isDark')).toBe('false');
+
+  fireEvent.click(darkModeToggle);
+  await new Promise(process.nextTick);
+  expect(document.documentElement.classList.contains('dark')).toBe(true);
+  expect(localStorage.isDark).toBe('true');
+
+  fireEvent.click(darkModeToggle);
+  await new Promise(process.nextTick);
+  expect(document.documentElement.classList.contains('dark')).toBe(false);
+  expect(localStorage.isDark).toBe('false');
 });
--- front/src/store/darkmodeSlice.js
+++ front/src/store/darkmodeSlice.js
@@ -1,16 +1,29 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+function setLocalStorage(isDark) {
+  localStorage.setItem('isDark', isDark ? 'true' : 'false');
+}
+
+function setDocumentElementClassList(isDark) {
+  if (isDark) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+}
+
 const isDark = JSON.parse(localStorage.getItem('isDark') || 'false');
+setLocalStorage(isDark);
+setDocumentElementClassList(isDark);
 
 export const toggle = createAsyncThunk(
   'darkmode/toggle',
   async (arg, thunkAPI) => {
     const { isDark } = thunkAPI.getState().darkmode;
-    if (isDark) {
-      document.documentElement.classList.remove('dark');
-    } else {
-      document.documentElement.classList.add('dark');
-    }
+
+    // Toggle darkmode, so let darkmode = !darkmode.
+    setLocalStorage(!isDark);
+    setDocumentElementClassList(!isDark);
   },
 );
 
