5827fb7641add15b4e0498016a9b56e739332034
Peterlits Zo <peterlitszo@gmail.com>
2022-01-27 13:19:56 +0800

    test: Add test for TWO dark mode toggle.
    To avoid two dark mode toggle works not well (because before those have TWO
    states), we use redux in commit 8c7c78cf5c59. So now we update the test for it
    to make sure that it works well.

--- front/src/component/Nav.test.jsx
+++ front/src/component/Nav.test.jsx
@@ -56,3 +56,33 @@ test('Nav all', async () => {
   expect(document.documentElement.classList.contains('dark')).toBe(false);
   expect(localStorage.isDark).toBe('false');
 });
+
+test('Two toggle, make sure they works well', async () => {
+  // Render it.
+  expect(localStorage.getItem('isDark')).toBe('false');
+  const { getAllByLabelText, getByText } = render(
+    <Provider store={store}>
+      <Nav />
+    </Provider>,
+  );
+
+  // Open menu to get the toggle in menu.
+  const menu = getByText('Menu');
+  fireEvent.click(menu);
+
+  // Whatever toggle we use, we still can change the dark mode.
+  const toggles = getAllByLabelText('Dark Mode');
+  expect(toggles.length >= 2).toBe(true);
+
+  let darkMode = true;
+  for (let i = 0; i < 100; i++) {
+    const index = Math.floor(Math.random() * toggles.length);
+    expect(toggles[index].nodeName).toBe('BUTTON');
+
+    fireEvent.click(toggles[index]);
+    await new Promise(process.nextTick);
+    expect(document.documentElement.classList.contains('dark')).toBe(darkMode);
+
+    darkMode = !darkMode;
+  }
+});
