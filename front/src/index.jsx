330ae54f2642fd0f8a45ee4a678e59a4ca7444e2
Peterlits Zo <peterlitszo@gmail.com>
2022-01-29 17:36:10 +0800

    refactor: Move `App` into `index.jsx`.

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App.jsx';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
 import { ApolloProvider } from '@apollo/client';
@@ -8,10 +7,27 @@ import { ApolloProvider } from '@apollo/client';
 import { client } from './client.js';
 import './index.css';
 
+import Index from './page/Index.jsx';
+import Nav from './component/Nav.jsx';
+import Footer from './component/Footer.jsx';
+
 if (process.env.NODE_ENV === 'development') {
   const { worker } = await import('./mock/browser.js');
   worker.start();
   console.log('Add mock...');
+}
+
+function App() {
+  return (
+    <div
+      className={`bg-first min-h-screen dark:text-green-100 text-green-900
+          flex flex-col`}
+    >
+      <Nav />
+      <Index />
+      <Footer />
+    </div>
+  );
 }
 
 ReactDOM.render(
