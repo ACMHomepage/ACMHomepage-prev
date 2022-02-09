22cf4a93ea43b5e54b5b99744e499dd9b408d520
Peterlits Zo <peterlitszo@gmail.com>
2022-02-10 00:11:50 +0800

    chore: Let `yarn build` build size graph by visualizer plugin.

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite';
+import visualizer from 'rollup-plugin-visualizer';
+
+const env = process.argv[process.argv.length - 1];
+
+const rollupPlugins = [];
+
+if (env === 'analyze') {
+  rollupPlugins.push(visualizer({
+    filename: './node_modules/.cache/visualizer/stats.html',
+    open: true,
+    gzipSize: true,
+    brotliSize: true,
+  }))
+}
 
 export default defineConfig({
   esbuild: {
     jsxFactory: 'jsx',
     jsxInject: `import { jsx } from 'theme-ui'`,
+  },
+  build: {
+    rollupOptions: {
+      plugins: rollupPlugins,
+    },
   },
 });
