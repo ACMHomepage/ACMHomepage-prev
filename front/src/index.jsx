98b0689f9bc54a41e95a5a3b7db8da177fe52658
Peterlits Zo <peterlitszo@gmail.com>
2022-02-12 15:21:26 +0800

    feat: Add preflight.
    [tailwindcss' preflight][tailwindcss].
    
    [tailwindcss]: https://tailwindcss.com/docs/preflight

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -19,20 +19,50 @@ if (process.env.NODE_ENV === 'development') {
   console.log('Add mock...');
 }
 
+/**
+ * tailwindcss's preflight style.
+ * @see {@link https://tailwindcss.com/docs/preflight}.
+ *
+ * Mount to set the style auto.
+ */
+const Preflight = () => (
+  <Global
+   styles={{
+     '*': {
+       transitionProperty: 'color, background-color, border-color, '
+         + 'text-decoration-color, fill, stroke, opacity, box-shadow, '
+         + 'transform, filter, backdrop-filter',
+       transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+       transitionDuration: '300ms',
+     },
+     'blockquote, dl, dd, h1, h2, h3, h4, h5, h6, hr, figure, p, pre': {
+       margin: 0,
+     },
+     'h1, h2, h3, h4, h5, h6': {
+       fontSize: 'inherit',
+       fontWeight: 'inherit',
+     },
+     'ol, ul': {
+       listStyle: 'none',
+       margin: 0,
+       padding: 0,
+     },
+     'img, svg, video, canvas, audio, iframe, embed, object': {
+       display: 'block',
+       verticalAlign: 'middle',
+     },
+     '*, ::before, ::after': {
+       borderWidth: 0,
+       borderStyle: 'solid',
+     },
+   }}
+ />
+);
+
 const AppWithTheme = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Global
-        styles={{
-          '*': {
-            transitionProperty: 'color, background-color, border-color, '
-              + 'text-decoration-color, fill, stroke, opacity, box-shadow, '
-              + 'transform, filter, backdrop-filter',
-            transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
-            transitionDuration: '300ms',
-          }
-        }}
-      />
+      <Preflight />
       <div
         sx={{
           color: 'text',
