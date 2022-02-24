6f07782ea61882e2df68ad82520ac777e600ee8f
Peterlits Zo <peterlitszo@gmail.com>
2022-02-24 18:09:52 +0800

    refactor: Remove useless darkmodeSlice.
    We let Theme-UI handle the dark mode. So we don't need darkmodeSlice anymore
    now.

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import darkmodeReducer from './darkmodeSlice';
 
 export default configureStore({
   reducer: {
-    darkmode: darkmodeReducer,
   },
 });
