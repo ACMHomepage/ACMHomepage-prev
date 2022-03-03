05c4e87e6ee489410ad2820f26be8b1ff64efd3f
Peterlits Zo <peterlitszo@gmail.com>
2022-03-03 17:08:12 +0800

    chore/style: Add tools to build docker container for backend.
    We add `Dockerfile` to define the docker image.
    
    We add `makefile` to build docker image, or run, remove the docker container,
    which will export a port to let host's application (aka, the file `main.js`)
    can connect with MariaDB (MySQL-like) server.
    
    We also add `init.sql` in folder `back/db/entrypoint/init.sql`, which will be
    used to init the database in docker container.
    
    By the way, I add some comment.

--- back/db/connection.js
+++ back/db/connection.js
@@ -1,5 +1,9 @@
 import mysql from "mysql2";
 
+/**
+ * MySQL connection object. Make sure that the values match the Dockerfile,
+ * which is used to create a MariaDB (MySQL-like) server.
+ */
 export const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
