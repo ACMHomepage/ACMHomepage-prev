5916523813000e6ceb57f20b00a6d408792f08e7
Peterlits Zo <peterlitszo@gmail.com>
2022-03-03 17:38:42 +0800

    style: Add comment for backend query.

--- back/db/query.js
+++ back/db/query.js
@@ -1,4 +1,8 @@
 import {conn} from "./connection.js"
 import util from "util";
 
+/**
+ * Promisify query, whose connection object is exported by file
+ * `back/db/connection.js` and binded to itself.
+ */
 export const query = util.promisify(conn.query).bind(conn)
