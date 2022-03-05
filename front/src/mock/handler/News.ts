3300f8ad6e6b4b1b522fecc29b63a52e80750394
Peterlits Zo <peterlitszo@gmail.com>
2022-03-05 19:38:02 +0800

    fix/refactor(front): Put GraphQL query statement into `newsSlice`, etc.
    Put these statements as hooks into `newsSlice.ts`, and make sure they match to
    the definetion of the backend.

--- front/src/mock/handler/News.ts
+++ front/src/mock/handler/News.ts
@@ -10,60 +10,60 @@ const data = {
     {
       id: 1,
       title: 'First',
-      image_uri: faker.image.abstract(undefined, undefined, true),
+      image_url: faker.image.abstract(undefined, undefined, true),
       content: 'First '.repeat(1000),
       summary: 'A lot of word `First`',
     },
     {
       id: 2,
       title: 'Second',
-      image_uri: faker.image.abstract(undefined, undefined, true),
+      image_url: faker.image.abstract(undefined, undefined, true),
       content: 'Second '.repeat(1000),
       summary: 'A lot of word `Second`',
     },
     {
       id: 3,
       title: 'White',
-      image_uri: faker.image.abstract(undefined, undefined, true),
+      image_url: faker.image.abstract(undefined, undefined, true),
       content: 'White '.repeat(1000),
       summary: 'Just white',
     },
     {
       id: 4,
       title: 'Random',
-      image_uri: faker.image.abstract(undefined, undefined, true),
+      image_url: faker.image.abstract(undefined, undefined, true),
       content: faker.lorem.paragraphs(100, '\n\n'), // markdown
       summary: 'Hello',
     },
   ],
 };
 
-const News = graphql.query('News', (req, res, ctx) => {
+const GetNews = graphql.query('GetNews', (req, res, ctx) => {
   const { newsId } = req.variables;
   const news = isUndefined(newsId)
     ? data.news
     : filter(data.news, (new_) => parseInt(newsId) === new_.id);
-  return res(ctx.data({ news }));
+  return res(ctx.data({ getNews: news }));
 });
 
 const CreateNews = graphql.mutation<CreateNewsData, CreateNewsVars>(
   'CreateNews',
   (req, res, ctx) => {
-    const { title, image_uri, content } = req.variables;
+    const { title, image_url, content } = req.variables;
     console.log(req.variables);
 
     data._id++;
     data.news.push({
       id: data._id,
       title,
-      image_uri,
+      image_url,
       content,
       summary: 'TODO',
     });
 
     return res(
       ctx.data({
-        news: {
+        createNews: {
           id: data._id,
         },
       }),
@@ -71,4 +71,4 @@ const CreateNews = graphql.mutation<CreateNewsData, CreateNewsVars>(
   },
 );
 
-export default [News, CreateNews];
+export default [GetNews, CreateNews];
--- front/src/store/newsSlice.ts
+++ front/src/store/newsSlice.ts
@@ -5,23 +5,22 @@ import { useLazyQuery, useMutation, gql } from '@apollo/client';
  *****************************************************************************/
 
 const CREATE_NEWS = gql`
-  # The image should be a base64 data string.
-  mutation CreateNews($title: String!, $image_uri: String!, $content: String!) {
-    news(title: $title, image_uri: $image_uri, content: $content) {
+  mutation CreateNews($title: String!, $image_url: String!, $content: String!) {
+    createNews(title: $title, image_url: $image_url, content: $content) {
       id
     }
   }
 `;
 
 export interface CreateNewsData {
-  news: {
+  createNews: {
     id: number;
   };
 }
 
 export interface CreateNewsVars {
   title: string;
-  image_uri: string;
+  image_url: string;
   content: string;
 }
 
@@ -37,11 +36,10 @@ export const useCreateNews = () => {
 
   const createNewsWithVars = (
     title: string,
-    image_uri: string,
+    image_url: string,
     content: string,
   ) => {
-    console.log({title, image_uri, content});
-    createNews({ variables: { title, image_uri, content } });
+    createNews({ variables: { title, image_url, content } });
   };
 
   return [createNewsWithVars, state] as [
@@ -49,3 +47,93 @@ export const useCreateNews = () => {
     typeof state,
   ];
 };
+
+/******************************************************************************
+ * Get news of a specific id.
+ *****************************************************************************/
+
+const GET_NEWS = gql`
+  query GetNews {
+    getNews {
+      id
+      title
+      content
+      image_url
+    }
+  }
+`;
+
+export interface GetNewsData {
+  getNews: {
+    id: number;
+    title: string;
+    content: string;
+    image_url: string;
+  }[];
+}
+
+export interface GetNewsVars {
+  id: number;
+}
+
+/**
+ * A hook to get news of a specific id. It will return a tuple with two element:
+ * - A function to get news.
+ * - A state about the process of creating news.
+ */
+export const useGetNews = () => {
+  const [getNews, state] = useLazyQuery<GetNewsData, GetNewsVars>(GET_NEWS);
+
+  const getNewsWithVars = (id: number) => {
+    getNews({ variables: { id } });
+  };
+
+  return [getNewsWithVars, state] as [typeof getNewsWithVars, typeof state];
+};
+
+/******************************************************************************
+ * Get news list.
+ *****************************************************************************/
+
+const GET_NEWS_LIST = gql`
+  query GetNews {
+    getNews {
+      id
+      title
+      image_url
+      summary
+    }
+  }
+`;
+
+export interface GetNewsListData {
+  getNews: {
+    id: number;
+    title: string;
+    image_url: string;
+    summary: string;
+  }[];
+}
+
+export interface GetNewsListVars {}
+
+/**
+ * A hook to get news list. It will return a tuple with two element:
+ * - A function to get news list.
+ * - A state about the process of creating news.
+ */
+export const useGetNewsList = () => {
+  const [getNewsList, state] = useLazyQuery<GetNewsListData, GetNewsListVars>(
+    GET_NEWS_LIST,
+  );
+  const getNewsListWithVars = () => {
+    console.log('before', state);
+    getNewsList();
+    console.log('after', state);
+  };
+  return [getNewsListWithVars, state] as [
+    typeof getNewsListWithVars,
+    typeof state,
+  ];
+};
+
