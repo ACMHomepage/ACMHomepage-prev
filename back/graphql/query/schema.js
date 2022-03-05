4939428d706accf4d876647b1794ee0d473c81ab
Peterlits Zo <peterlitszo@gmail.com>
2022-03-05 14:29:27 +0800

    refactor(back): Refactor news interface.
    I move the definition of the news type into `graphql/type/news.js` rather than
    `graphql/data_types/dataTypes.js`. I also move the query field `getNews` and
    `createNews` into `graphql/type/news.js`.
    
    I remove `graphql/query/query.js` because it is too short now to put it in a
    single file. I move it into `schema.js` (whose folder is from `graphql/query/`
    into `graphql/`, and this change make me also change file `main.js`'s import
    statement).
    
    I am not sure why there need be a `readme.md` in `graphql`. So I remove it (It
    will be a better choice that put it into `doc` folder of the project root).

--- back/graphql/query/schema.js
+++
@@ -1,7 +0,0 @@
-import { GraphQLSchema as GraphQLSchemaCreator } from "graphql";
-import { QueryType, MutationType } from "./query.js";
-
-export const GraphQLSchema = new GraphQLSchemaCreator({
-  query: QueryType,
-  mutation: MutationType,
-});
---
+++ back/graphql/schema.js
@@ -0,0 +1,21 @@
+import { GraphQLSchema as GraphQLSchemaCreator, GraphQLObjectType } from "graphql";
+import { createNews, getNews } from './type/news.js';
+
+export const QueryType = new GraphQLObjectType({
+  name: 'query',
+  fields: {
+    getNews
+  }
+});
+
+export const MutationType  = new GraphQLObjectType({
+  name: "createNews",
+  fields: {
+    createNews,
+  },
+});
+
+export const GraphQLSchema = new GraphQLSchemaCreator({
+  query: QueryType,
+  mutation: MutationType,
+});
