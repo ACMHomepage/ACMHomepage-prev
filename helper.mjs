5355e7ab0f3a22d312063f1f7e8e603f979e5b45
Peterlits Zo <peterlitszo@gmail.com>
2022-03-06 11:25:10 +0800

    chore: Add helper.mjs.
    `helper.mjs` is a zx script. It is useful if I want to find a word.
    
    Now I want to rename `doc` folder to `docs` folder because
    [github-PR-template][] need it, so I can run to also change document with the
    new name:
    ```shell
    $ ./helper.mjs find_all_file | xargs egrep "\<doc\>"
    ./doc/contribute.md:  - In `doc` folder, there are the other documents.
    ./README.md:[Click](./doc/contribute.md) to see how to contribute.
    ```
    
    [github-PR-template]: https://docs.github.com/en/communities/using-templates-to-encourage-useful-issues-and-pull-requests/creating-a-pull-request-template-for-your-repository

---
+++ helper.mjs
@@ -0,0 +1,39 @@
+#!/usr/bin/env zx
+// Using `yarn global add zx`, and make sure that `yarn global bin` is in your
+// PATH env variable.
+
+// The first 3 arguments is: node's path, zx's path and this file's path.
+// Remove to get the really arguments.
+const [node_path, zs_path, this_file_path, ...argv] = process.argv;
+
+$.verbose = false;
+
+if (argv.length === 1 && argv[0] === 'find_all_file') {
+  const ignore_regex = [
+    '.*/node_modules/.*',
+    '.*/\\.git/.*',
+    '.*/dist/.*',
+  
+    '.*/yarn\\.lock',
+    '.*/package-lock\\.json',
+  ];
+  await $`find . -type f ${
+    (() => {
+      const result = []
+      ignore_regex.forEach(value => {
+        result.push('!');
+        result.push('-regex');
+        result.push(value);
+      })
+      return result;
+    })()
+  }`.pipe(process.stdout)
+} else {
+  console.log([
+    'Usage: ./helper.mjs <command>',
+    '',
+    'Command:',
+    '  find_all_file  Show all files of the pwd row by row.',
+    '  help           Show this message',
+  ].join('\n'))
+}
