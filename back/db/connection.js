af305046ad4828f3b52c8264a04f297bb72a397c
Peterlits Zo <peterlitszo@gmail.com>
2022-03-09 10:04:40 +0800

    chore/style: Let project own a single prettierrc, and run `yarn format`.
    We put the `front/.prettierrc` into `.prettierrc` at the root folder of this
    project. And change two `package.json`(in folder `front` and `back`) to format
    with a single config file.
    
    And run `yarn format`.

--- back/db/connection.js
+++ back/db/connection.js
@@ -1,13 +1,13 @@
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
 
 /**
  * MySQL connection object. Make sure that the values match the Dockerfile,
  * which is used to create a MariaDB (MySQL-like) server.
  */
 export const conn = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "******",
-  database: "acmhomepage",
-  charset: "utf8mb4", // most 4 bytes, so emoji is included.
+  host: 'localhost',
+  user: 'root',
+  password: '******',
+  database: 'acmhomepage',
+  charset: 'utf8mb4', // most 4 bytes, so emoji is included.
 });
