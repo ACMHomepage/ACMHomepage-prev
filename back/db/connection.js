434c24d5a3930272af23cb08b75df654085f08ff
Peterlits Zo <peterlitszo@gmail.com>
2022-03-10 15:03:13 +0800

    chore: Add docker-compose. Now whole project can run in docker.
    To let whold project can run in docker containers, we:
    
    * Add retry loop in backend. If backend cannot create connection with database,
      it will retry rather than throw an error.
    * Let frontend use uri `/graphql` to communicate with backend rather than
      another port. (And that's why we need nginx)
    * Add `Dockerfile` for nginx, a server and which is the only entrypoint for
      users. (It will help us send static file and as a proxy for backend, in just
      one port 80)
    
    And we put those in one `docker-compose.yml`. And we can just run
    `docker-compose up` to start.

--- back/db/connection.js
+++ back/db/connection.js
@@ -4,10 +4,21 @@ import mysql from 'mysql2/promise';
  * MySQL connection object. Make sure that the values match the Dockerfile,
  * which is used to create a MariaDB (MySQL-like) server.
  */
-export const conn = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '******',
-  database: 'acmhomepage',
-  charset: 'utf8mb4', // most 4 bytes, so emoji is included.
-});
+export const conn = await (async () => {
+  while (true) {
+    try {
+      return await mysql.createConnection({
+        // Check `docker-compose.yml` in the root of project.
+        host: 'db',
+        user: 'root',
+        password: '******',
+        database: 'acmhomepage',
+        charset: 'utf8mb4', // most 4 bytes, so emoji is included.
+      });
+    } catch (e) {
+      // Retry after 5s.
+      console.log('Cannot create connection, retry...', e);
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+  }
+})()
