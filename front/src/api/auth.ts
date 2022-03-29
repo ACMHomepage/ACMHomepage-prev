b74ac71e84725a84fb5d1d45f42eb3ba55611bfb
Peterlits Zo <peterlitszo@gmail.com>
2022-03-29 17:02:54 +0800

    Refactor(front): Add `api` folder for user hooks
    There were some user hooks, for example, `useSignIn` and `useSignUp`, which are
    using for handle user event (talk with GraphQL server, and change redux store)
    before.
    
    Now I move those functions from the file `front/src/store/authSlice.ts` to the
    file `front/src/api/auth.ts`. I also add a hook named `useSignOut` to sign out.
    
    Then I change the component `SignButton`, page `Register` and `SignIn` to use
    the new hooks.
    
    I even change the msw mock handlers, in the file `Auth.ts`.

---
+++ front/src/api/auth.ts
@@ -0,0 +1,222 @@
+import { useMutation, gql } from '@apollo/client';
+
+import { useDispatch } from '../hooks';
+import {
+  signInWithInfo,
+  signInAndLoading,
+  unloggedWithError,
+  signOut,
+} from '../store/authSlice';
+import type { UserData } from '../store/authSlice';
+
+/******************************************************************************
+ * Sign in part
+ *****************************************************************************/
+
+export const SIGN_IN_MUTATION_NAME = 'SignIn';
+
+export const SIGN_IN_MUTATION = gql`
+  mutation ${SIGN_IN_MUTATION_NAME}($email: String!, $password: String!) {
+    # *************************************************************************
+    # WARING: Remember to change its interface if you change the code below.
+    # *************************************************************************
+
+    # Input email, password, and output user's data.
+    signIn(email: $email, password: $password) {
+      nickname # string
+      email # string
+      isAdmin # boolean
+    }
+  }
+`;
+
+export interface SignInData {
+  signIn: UserData;
+}
+
+export interface SignInVars {
+  email: string;
+  password: string;
+}
+
+/**
+ * Hook useSignIn to sign in. It will talk to GraphQL server and update the
+ * redux store auto. That means you should use another hook `useSelector` from
+ * redux to get the state.
+ *
+ * @example
+ * ```typescript
+ * const signIn = useSignIn();
+ * // the function `selectAuthState` is from the file named
+ * // `front/src/store/authSlice.ts`.
+ * const authState = useSelector(selectAuthState);
+ *
+ * return (
+ *   <button onClick={signIn(email, password)}>
+ *     {
+ *       // Remember it do not hold all state!
+ *       authState === AuthStateEnum.LoggedWithInfo
+ *         ? 'Logged'
+ *         : 'Unlogged'
+ *     }
+ *   </button>
+ * )
+ * ```
+ */
+export const useSignIn = () => {
+  const [signIn, { data, error, loading }] = useMutation<
+    SignInData,
+    SignInVars
+  >(SIGN_IN_MUTATION);
+  const dispatch = useDispatch();
+
+  if (data) {
+    dispatch(signInWithInfo(data.signIn));
+  } else if (loading) {
+    dispatch(signInAndLoading());
+  } else if (error) {
+    dispatch(unloggedWithError());
+  }
+
+  return (email: string, password: string) =>
+    signIn({ variables: { email, password } });
+};
+
+/******************************************************************************
+ * Register part
+ *****************************************************************************/
+
+export const REGISTER_MUTATION_NAME = 'Register';
+
+export const REGISTER_MUTATION = gql`
+  mutation ${REGISTER_MUTATION_NAME}
+      ($nickname: String!, $email: String!, $password: String!) {
+    # *************************************************************************
+    # WARING: Remember to change its interface if you change the code below.
+    # *************************************************************************
+
+    # Input nickname, email, password, and output its data.
+    register(nickname: $nickname, email: $email, password: $password) {
+      nickname # string
+      email # string
+      isAdmin # string
+    }
+  }
+`;
+
+export interface RegisterData {
+  register: UserData;
+}
+
+export interface RegisterVars {
+  nickname: string;
+  email: string;
+  password: string;
+}
+
+/**
+ * Hook useRegister to register. It will talk to GraphQL server and update the
+ * redux store auto. That means you should use another hook `useSelector` from
+ * redux to get the state. It will sign in auto.
+ *
+ * @todo
+ * Now back-end cannot sign in auto. Please let back-end set httponly cookie!
+ *
+ * @example
+ * ```typescript
+ * const register = useRegister();
+ * // the function `selectAuthState` is from the file named
+ * // `front/src/store/authSlice.ts`.
+ * const authState = useSelector(selectAuthState);
+ *
+ * return (
+ *   <button onClick={register(email, password)}>
+ *     {
+ *       // Remember it do not hold all state!
+ *       authState === AuthStateEnum.LoggedWithInfo
+ *         ? 'Logged'
+ *         : 'Unlogged'
+ *     }
+ *   </button>
+ * )
+ * ```
+ */
+
+export const useRegister = () => {
+  const [register, { loading, error, data }] = useMutation<
+    RegisterData,
+    RegisterVars
+  >(REGISTER_MUTATION);
+  const dispatch = useDispatch();
+
+  if (data) {
+    dispatch(signInWithInfo(data.register));
+  } else if (loading) {
+    dispatch(signInAndLoading());
+  } else if (error) {
+    dispatch(unloggedWithError());
+  }
+
+  return (nickname: string, email: string, password: string) =>
+    register({ variables: { nickname, email, password } });
+};
+
+/******************************************************************************
+ * Sign out part
+ *****************************************************************************/
+
+export const SIGN_OUT_MUTATION_NAME = 'SignOut';
+
+export const SIGN_OUT_MUTATION = gql`
+  mutation ${SIGN_OUT_MUTATION_NAME} {
+    # *************************************************************************
+    # WARING: Remember to change its interface if you change the code below.
+    # *************************************************************************
+    signOut
+  }
+`;
+
+export interface SignOutData {
+  signOut: boolean; // The type is not matter.
+}
+
+/**
+ * Hook useSignOut to sign out. It will talk to GraphQL server and update the
+ * redux store auto. That means you should use another hook `useSelector` from
+ * redux to get the state. It will sign in auto.
+ *
+ * @todo
+ * If the application is start, please sign out auto if the state is `Unlogged`
+ * or something else.
+ *
+ * @example
+ * ```typescript
+ * const signOut = useSignOut();
+ * // the function `selectAuthState` is from the file named
+ * // `front/src/store/authSlice.ts`.
+ * const authState = useSelector(selectAuthState);
+ *
+ * return (
+ *   <button onClick={signOut()}>
+ *     {
+ *       // Remember it do not hold all state!
+ *       authState === AuthStateEnum.LoggedWithInfo
+ *         ? 'Logged'
+ *         : 'Unlogged'
+ *     }
+ *   </button>
+ * )
+ * ```
+ */
+
+export const useSignOut = () => {
+  const [signOutFunction, { data }] =
+    useMutation<SignOutData>(SIGN_OUT_MUTATION);
+  const dispatch = useDispatch();
+
+  if (data) {
+    dispatch(signOut());
+  }
+
+  return signOutFunction;
+};
--- front/src/store/authSlice.ts
+++ front/src/store/authSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { useLazyQuery, useMutation, gql } from '@apollo/client';
+import { useMutation, gql } from '@apollo/client';
 
 import type { RootState } from './store';
 import { useDispatch } from '../hooks';
@@ -13,18 +13,22 @@ const IS_LOGGED_ITEM_KEY = `store-${SLICE_NAME}-isLogged`;
 /******************************************************************************
  * State types
  *****************************************************************************/
+export interface UserData {
+  nickname: string;
+  email: string;
+  isAdmin: boolean;
+}
+
 export enum AuthStateEnum {
   LoggedWithInfo,
   LoggedWithoutInfo,
   LoggedAndLoadingInfo,
   Unlogged,
+  UnloggedWithError,
 }
 
-interface LoggedWithInfo {
+interface LoggedWithInfo extends UserData {
   state: AuthStateEnum.LoggedWithInfo;
-  nickname: string;
-  email: string;
-  isAdmin: boolean;
 }
 
 interface LoggedWithoutInfo {
@@ -39,11 +43,16 @@ interface Unlogged {
   state: AuthStateEnum.Unlogged;
 }
 
+interface UnloggedWithError {
+  state: AuthStateEnum.UnloggedWithError;
+}
+
 type AuthState =
   | LoggedWithInfo
   | LoggedWithoutInfo
   | LoggedAndLoadingInfo
-  | Unlogged;
+  | Unlogged
+  | UnloggedWithError;
 
 /******************************************************************************
  * Main part
@@ -55,12 +64,8 @@ const initialState = ((): AuthState => {
   const isLogged = !!localStorage.getItem(IS_LOGGED_ITEM_KEY);
 
   return isLogged
-    ? {
-        state: AuthStateEnum.LoggedWithoutInfo,
-      }
-    : {
-        state: AuthStateEnum.Unlogged,
-      };
+    ? { state: AuthStateEnum.LoggedWithoutInfo }
+    : { state: AuthStateEnum.Unlogged };
 })();
 
 export const authSlice = createSlice({
@@ -71,10 +76,7 @@ export const authSlice = createSlice({
       localStorage.removeItem(IS_LOGGED_ITEM_KEY);
       state.state = AuthStateEnum.Unlogged;
     },
-    signInWithInfo: (
-      _state: AuthState,
-      actions: PayloadAction<Omit<LoggedWithInfo, 'state'>>,
-    ) => {
+    signInWithInfo: (_state: AuthState, actions: PayloadAction<UserData>) => {
       localStorage.setItem(IS_LOGGED_ITEM_KEY, 'true');
       return {
         state: AuthStateEnum.LoggedWithInfo,
@@ -89,137 +91,23 @@ export const authSlice = createSlice({
       localStorage.removeItem(IS_LOGGED_ITEM_KEY);
       state.state = AuthStateEnum.LoggedWithoutInfo;
     },
+    unloggedWithError: (state: AuthState) => {
+      localStorage.removeItem(IS_LOGGED_ITEM_KEY);
+      state.state = AuthStateEnum.UnloggedWithError;
+    },
   },
 });
 
 /******************************************************************************
- * Sign in part
- *****************************************************************************/
-
-const SIGN_IN = gql`
-  mutation SignIn($email: String!, $password: String!) {
-    # *************************************************************************
-    # WARING: Remember to change its interface if you change the code below.
-    # *************************************************************************
-
-    # Input email, password, and output user's data.
-    signIn(email: $email, password: $password) {
-      nickname
-      email
-      isAdmin
-    }
-  }
-`;
-
-interface SignInData {
-  signIn: {
-    nickname: string;
-    email: string;
-    isAdmin: boolean;
-  };
-}
-
-interface SignInVars {
-  email: string;
-  password: string;
-}
-
-/**
- * Hook useSignIn to sign in.
- *
- * Usage:
- * ```typescript
- * const signIn = useSignIn();
- * // selectAuthState is from the same file.
- * const authState = useSelector(selectAuthState);
- *
- * return (
- *   <button onClick={signIn(email, password)}>
- *     {
- *       // Remember it do not hold all state!
- *       authState === AuthStateEnum.LoggedWithInfo
- *         ? 'Logged'
- *         : 'Unlogged'
- *     }
- *   </button>
- * )
- * ```
- */
-export const useSignIn = () => {
-  const [signIn, { data, error, loading }] = useMutation<
-    SignInData,
-    SignInVars
-  >(SIGN_IN);
-  const dispatch = useDispatch();
-
-  if (data) {
-    dispatch(signInWithInfo(data.signIn));
-  } else if (loading) {
-    dispatch(signInAndLoading());
-  } else if (error) {
-    // do nothing... now
-    // TODO: Add error message for user. And UnloggedWithError state.
-  }
-
-  return (email: string, password: string) =>
-    signIn({ variables: { email, password } });
-};
-
-/******************************************************************************
- * Sign up part
- *****************************************************************************/
-
-// be mocked in file `front/src/mock/handler/Auth.ts`.
-const REGISTER = gql`
-  mutation Register($nickname: String!, $email: String!, $password: String!) {
-    # *************************************************************************
-    # WARING: Remember to change its interface if you change the code below.
-    # *************************************************************************
-
-    # Input nickname, email, password, and output its data.
-    register(nickname: $nickname, email: $email, password: $password) {
-      nickname
-      email
-      isAdmin
-    }
-  }
-`;
-
-export interface RegisterData {
-  register: {
-    nickname: string;
-    email: string;
-    isAdmin: boolean;
-  };
-}
-
-export interface RegisterVars {
-  nickname: string;
-  email: string;
-  password: string;
-}
-
-export const useRegister = () => {
-  // TODO: deal with loading, error, and data.
-  const [register, { loading, error, data }] = useMutation<
-    RegisterData,
-    RegisterVars
-  >(REGISTER);
-  const dispatch = useDispatch();
-
-  if (data) {
-    dispatch(signInWithInfo(data.register));
-  }
-
-  return (nickname: string, email: string, password: string) =>
-    register({ variables: { nickname, email, password } });
-};
-
-/******************************************************************************
  * Export others
  *****************************************************************************/
-export const { signOut, signInWithInfo, signInWithoutInfo, signInAndLoading } =
-  authSlice.actions;
+export const {
+  signOut,
+  signInWithInfo,
+  signInWithoutInfo,
+  signInAndLoading,
+  unloggedWithError,
+} = authSlice.actions;
 
 /**
  * Get the auth's data.
