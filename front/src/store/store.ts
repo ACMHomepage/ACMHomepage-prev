2e06275e7b5e2f4e5f05a4954bf7ce2e6dc0eb5b
Peterlits Zo <peterlitszo@gmail.com>
2022-04-18 21:12:22 +0800

    feat(front): Add the theme mode
    We want to remove Theme-UI. So we have to deal with theme mode by ourselve.

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './authSlice';
+import themeModeSlice from './themeModeSlice';
 
 const store = configureStore({
   reducer: {
     auth: authSlice,
+    themeMode: themeModeSlice,
   },
 });
 
