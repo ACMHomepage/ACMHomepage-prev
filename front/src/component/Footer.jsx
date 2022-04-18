60f4dc08c4d689cdbdfcb5cf00ebad1ca38db3ed
Peterlits Zo <peterlitszo@gmail.com>
2022-04-18 21:15:28 +0800

    feat(front): Update the component `Footer`
    To update its style.

--- front/src/component/Footer.jsx
+++ front/src/component/Footer.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { merge } from 'lodash';
-import { flexbox, bg, spacing } from '@acm-homepage/theme-shortcut';
 
-import { footer, utilMainPart } from '../config';
+import { footer } from '../config';
+
+import styles from './styles/Footer.module.scss';
 
 export default function App() {
   return (
     <React.Fragment>
-      <div sx={flexbox({ flex: 1 })} />
-      <div
-        sx={merge(
-          bg({ col: 'secondaryBackground' }),
-          spacing({ m: { t: '4rem' }, p: { b: '4rem', t: '1rem' } }),
-        )}
-      >
-        <div sx={{ ...utilMainPart }}>{footer}</div>
+      <div className={styles.Space} />
+      <div className={styles.Footer}>
+        <div className={styles.Main}>{footer}</div>
       </div>
     </React.Fragment>
   );
