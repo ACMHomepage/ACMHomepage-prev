0a6e49640b6b4cdfba19fd5bcdff17a21e248a41
Peterlits Zo <peterlitszo@gmail.com>
2022-04-27 22:09:15 +0800

    refactor(front): Update Nav
    Now the `Nav` is using CSS Module rather than using CSS-in-JS.

--- front/src/component/Nav/PostNews.tsx
+++ front/src/component/Nav/PostNews.tsx
@@ -1,32 +1,17 @@
-import { useNavigate } from 'react-router-dom';
-import { Send } from 'lucide-react';
-import { size, bg, flexbox } from '@acm-homepage/theme-shortcut';
-import { merge } from 'lodash';
-
 import { URL as postNewsUrl } from '../../page/PostNews';
 
-import Button from './../Button';
+import LinkButton from './../LinkButton';
 
-const PostNews = ({ className }: { className?: string }) => {
-  const navigate = useNavigate();
+import styles from './styles/PostNews.module.scss';
 
+const PostNews = () => {
   return (
-    <Button
-      sx={merge(
-        size({ h: '2rem' }),
-        bg({ col: { _: 'bg-2', hv: 'bg-4' } }),
-        flexbox({
-          dir: 'row',
-          gap: '0.25rem',
-          place: { content: 'center', items: 'center' },
-        }),
-      )}
-      className={className}
-      onClick={() => navigate(postNewsUrl)}
+    <LinkButton
+      to={postNewsUrl}
+      className={styles.button}
     >
-      <Send size={16} />
       Post News
-    </Button>
+    </LinkButton>
   );
 };
 
