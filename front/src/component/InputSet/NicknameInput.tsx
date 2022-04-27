14b7a1f2ca2d22f35f3ba1538ed9f28bb671ea81
Peterlits Zo <peterlitszo@gmail.com>
2022-04-27 21:49:36 +0800

    feat(front): Support the page `Register`

--- front/src/component/InputSet/NicknameInput.tsx
+++ front/src/component/InputSet/NicknameInput.tsx
@@ -1,20 +1,20 @@
 import { User } from 'lucide-react';
 import Input from './Input';
 
-const NickNameInput = (props: {
-  nickname: string;
-  setNickname: (nickname: string) => void;
-}) => {
-  const { nickname, setNickname } = props;
+import type { InputProps } from './Input';
+
+import styles from './styles/NicknameInput.module.scss';
+
+const NickNameInput = (props: InputProps) => {
+  const { onChange } = props;
 
   return (
     <Input.container>
-      <User sx={Input.sx.startIcon()} />
+      <User className={styles.user} />
       <input
         placeholder="Nickname"
-        sx={Input.sx.input({ hasStartIcon: true })}
-        value={nickname}
-        onChange={(event) => setNickname(event.target.value)}
+        className={styles.nicknameInput}
+        onChange={onChange}
       />
     </Input.container>
   );
