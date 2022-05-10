cfb0f779bb00786c582032be6fc1c361c33c2450
Riley Ye <rileyye@qq.com>
2022-05-10 21:01:03 +0800

    feat(back): Complete the back-end interface for crawler data storage
    In this commit, I made the back-end enable to store the data from the crawler.
    
    To extend, I created 2 new tables named `userAccount` and `problemSolved`.
    `UserAccount` refers to the user-account relation and `problemSolved` intends
    to store the contest data for the account.
    
    I also made the back-end support `getUserAccount`, `getSovled`,
    `insertUserAccount`, `removeUserAccount`, `insertSolved` and `removeSolved`!

---
+++ back/src/graphql/type/userAccount.ts
@@ -0,0 +1,97 @@
+import {
+    GraphQLInt,
+    GraphQLList,
+    GraphQLObjectType,
+    GraphQLString,
+} from 'graphql';
+
+/******************************************************************************
+ * Main part
+ *****************************************************************************/
+export const UserAccountType = new GraphQLObjectType({
+    name: 'userAccount',
+    fields: {
+        id: {
+            type: GraphQLInt,
+            description: 'the user id'
+        },
+        account: {
+            type: GraphQLString,
+            description: 'the contest account'
+        },
+        source: {
+            type: GraphQLString,
+            description: 'the contest website/name'
+        }
+    },
+});
+
+/******************************************************************************
+ * Query field.
+ *****************************************************************************/
+export const getUserAccount = (database) => ({
+    type: GraphQLList(UserAccountType),
+    args: {
+        id: {
+            type: GraphQLInt,
+            description: 'the user id'
+        }
+    },
+    async resolve(_parentVal, args) {
+        if (args.id === null) {
+            return await database.userAccount.getAll();
+        }
+        return await database.userAccount.getByID(args.id);
+    }
+});
+
+/******************************************************************************
+ * Mutation field.
+ *****************************************************************************/
+export const insertUserAccount = (database) => ({
+    type: UserAccountType,
+    args: {
+        id: {
+            type: GraphQLInt,
+            description: 'the user id'
+        },
+        account: {
+            type: GraphQLString,
+            description: 'the contest account'
+        },
+        source: {
+            type: GraphQLString,
+            description: 'the contest website/name'
+        }
+    },
+    async resolve(_parentVal, args) {
+        return await database.userAccount.insert(args.id, args.account, args.source);
+    }
+});
+
+export const removeUserAccount = (database) => ({
+    type: UserAccountType,
+    args: {
+        id: {
+            type: GraphQLInt,
+            description: 'the user id'
+        },
+        account: {
+            type: GraphQLString,
+            description: 'the contest account'
+        },
+        source: {
+            type: GraphQLString,
+            description: 'the contest website/name'
+        }
+    },
+    async resolve(_parentVal, args) {
+        if (args.account === undefined && args.source === undefined) {
+            return await database.userAccount.removeByID(args.id);
+        } else if (args.account !== undefined && args.source !== undefined) {
+            return await database.userAccount.remove(args.id, args.account, args.source)
+        } else {
+            return null;
+        }
+    }
+});
