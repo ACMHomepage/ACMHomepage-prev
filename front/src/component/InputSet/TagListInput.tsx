e351dcff32abe0c490482746e6784863f12d1f25
CNTTDragon <137513156@qq.com>
2022-05-18 21:27:36 +0800

    feat(front): Support add taglist in postnews
    In this commit

---
+++ front/src/component/InputSet/TagListInput.tsx
@@ -0,0 +1,22 @@
+import { Tag } from 'lucide-react';
+import Input from './Input';
+import type { InputProps } from './Input';
+
+import styles from './styles/EmailInput.module.scss';//
+
+const EmailInput = (props: InputProps) => {
+  const { onChange } = props;
+
+  return (
+    <Input.container>
+      <Tag className={styles.mail} />
+      <input
+        placeholder="TagList"
+        className={styles.emailInput}
+        onChange={onChange}
+      />
+    </Input.container>
+  );
+};
+
+export default EmailInput;
---
+++ front/src/util/toTagList.ts
@@ -0,0 +1,7 @@
+const toTagList = (tagList: string): string[] => {
+    return tagList.split(',')
+        .map(tag => tag.trim())
+        .filter(tag => tag !== '');
+}
+
+export default toTagList;
